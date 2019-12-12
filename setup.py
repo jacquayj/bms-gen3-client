982c2c70978606cdbc153e39e94e8150efa22317
John Jacquay <john@bioteam.net>
2019-12-11 19:57:43 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bms-gen3-client",
-    version="0.0.1",
+    version="0.0.2",
     description="Gen3 client wrapper for HTTP basic auth and eternal API keys for BMS",
     url="https://github.com/jacquayj/bms-gen3-client", # Need to rehost at GitHub
     license="Apache",
