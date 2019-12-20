dc2c9593021d846ab85efa3db61b7f10a32ef92d
John Jacquay <john@bioteam.net>
2019-12-20 15:19:41 -0700

    Fixed deps not being installed

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="bms-gen3-client",
     version="0.0.4",
     description="Gen3 client wrapper for HTTP basic auth and eternal API keys for BMS",
     url="https://github.com/jacquayj/bms-gen3-client", # Need to rehost at GitHub
     license="Apache",
-    # entry_points={
-    #     'console_scripts': [
-    #         'bms_load_projects=bms_project_morph:load_projects',
-    #     ],
-    # },
+    install_requires=requirements,
     packages=find_packages(),
 )
